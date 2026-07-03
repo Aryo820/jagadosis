98c9130aca64ad24e639ec8e7595dbd7696718b2
Aryo820 <aryopramudityas97@gmail.com>
2026-07-03 15:37:47 +0700

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:aplikasi/database/preference_handler.dart';
+import 'package:aplikasi/firebase_options.dart';
 import 'package:aplikasi/screens/auth/login_page.dart';
 import 'package:aplikasi/screens/dashboard_page.dart';
 import 'package:aplikasi/screens/splash/splash_screen.dart';
 import 'package:aplikasi/services/notification_service.dart';
 import 'package:aplikasi/utils/app_colors.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting('id_ID', null);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await PreferenceHandler.init();
 
   // Initialize notification service and reschedule existing reminders
